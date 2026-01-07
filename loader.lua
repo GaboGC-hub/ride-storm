57a1dfbc4e42d5ae450d7bdf352d0c76345df489
gabogc-hub <josegabrielgutierrezcaicedo@gmail.com>
2026-01-07 17:06:37 -0500

    Refactor loader to load hub.lua directly

--- loader.lua
+++ loader.lua
@@ -1,12 +1,3 @@
--- RideStorm Loader
-if getgenv().RideStormLoaded then return end
-getgenv().RideStormLoaded = true
-
-local base = "https://raw.githubusercontent.com/GaboGC-hub/ride-storm/main/"
-
-local function load(path)
-    local src = game:HttpGet(base .. path)
-    return loadstring(src)()
-end
-
-load("core/init.lua")
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/GaboGC-hub/ride-storm/main/hub.lua"
+))()

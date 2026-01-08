135ab7231a9509c48bfb0537a1e3ace72ee52a08
gabogc-hub <josegabrielgutierrezcaicedo@gmail.com>
2026-01-08 01:32:07 -0500

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -1,1 +1,22 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local player = Players.LocalPlayer
+
+-- ANTI AFK
+local vu = game:GetService("VirtualUser")
+player.Idled:Connect(function()
+    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
+
+-- NOCLIP PERFECTO
+RunService.Stepped:Connect(function()
+    if not player.Character then return end
+    for _, v in ipairs(player.Character:GetDescendants()) do
+        if v:IsA("BasePart") then
+            v.CanCollide = false
+        end
+    end
+end)
 

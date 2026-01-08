b5c531851019ba18b1e62e4f69d77f5a43b7ff4c
gabogc-hub <josegabrielgutierrezcaicedo@gmail.com>
2026-01-07 23:06:19 -0500

    Adjust pickup and job delivery positions in autofarm

--- autofarm.lua
+++ autofarm.lua
@@ -15,13 +15,13 @@ local jobPrompt = jobPart:WaitForChild("ProximityPrompt")
 
 while RS.Farming do
     -- ir a recoger
-    hrp.CFrame = pickupBox.CFrame + Vector3.new(0,3,0)
+    hrp.CFrame = pickupBox.CFrame + Vector3.new(0,5,0)
     task.wait(0.15)
     fireproximityprompt(pickupPrompt, 1)
 
     -- entregar
-    hrp.CFrame = jobPart.CFrame + Vector3.new(0,3,0)
-    task.wait(3)
+    hrp.CFrame = jobPart.CFrame + Vector3.new(0,5,0)
+    task.wait(0.15)
     fireproximityprompt(jobPrompt, 1)
 
     -- dinero estimado
